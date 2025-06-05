0767c1da30d4546ccb8a2fcba7e5a931d638b1bf
Guilherme Corá <guilhermearcora@gmail.com>
2025-06-05 02:27:09 -0300

    feat(security) add data sanitization middlewares [xss, express-mongo-sanitize]

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const xss = require('xss-clean');
+const mongoSanitize = require('express-mongo-sanitize');
 const app = express();
 
 const globalErrorHandler = require('./controllers/errorController');
@@ -26,6 +28,12 @@ app.use('/api', limiter);
 
 // Body parser, reading data from body into req.body and limiting the amount of data coming.
 app.use(express.json({limit: '10kb' }));
+
+//Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS, html malicious code
+app.use(xss());
 
 // Routes
 app.use('/api/v1', apiRouter);
