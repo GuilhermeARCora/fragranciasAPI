6d8e1d125182fcf1d8ada8143769acdc7833ea1f
Guilherme Corá <guilhermearcora@gmail.com>
2025-07-02 23:38:48 -0300

    feat: finishing products CRUD

--- daos/productDao.js
+++ daos/productDao.js
@@ -7,6 +7,41 @@ const createOneProduct = async(data) =>{
   return product;
 };
 
+const getAllProducts = async() =>{
+
+  const product = await Product.find();
+
+  return product;
+};
+
+const getOneProduct = async(id) =>{
+
+  const product = await Product.findById(id);
+
+  return product;
+};
+
+const editOneProduct = async({id, ...updates}) =>{
+
+  const product = await Product.findByIdAndUpdate(id, updates, {
+        new: true,
+        runValidators: true
+    });
+
+  return product;
+};
+
+const deleteOneProduct = async(id) =>{
+
+  const deleted = await Product.findByIdAndDelete(id);
+
+  return deleted;
+};
+
 module.exports = {
-  createOneProduct
+  createOneProduct,
+  getAllProducts,
+  getOneProduct,
+  editOneProduct,
+  deleteOneProduct
 };
--- services/productService.js
+++ services/productService.js
@@ -1,4 +1,5 @@
 const productDao = require('../daos/productDao');
+const AppError = require('../utils/appError');
 const filterFields = require('../utils/filterFields');
 
 const createOneProduct = async(reqBodyObj) =>{
@@ -10,6 +11,52 @@ const createOneProduct = async(reqBodyObj) =>{
   return createdProduct;
 };
 
+const getAllProducts = async() =>{
+
+  const products = await productDao.getAllProducts();
+
+  return products;
+};
+
+const getOneProduct = async(id) =>{
+
+  const product = await productDao.getOneProduct(id);
+  
+  if(!product){
+    throw new AppError("This products Id does not exist", 404)
+  };
+  
+  return product;
+};
+
+const editOneProduct = async(reqParamsId, body) =>{
+
+  const id = reqParamsId;
+
+  const product = await productDao.editOneProduct({id, ...body});
+  
+  if(!product){
+    throw new AppError("This products Id does not exist", 404)
+  };
+  
+  return product;
+};
+
+const deleteOneProduct = async(id) =>{
+
+  const deleted = await productDao.deleteOneProduct(id);
+  
+  if(!deleted){
+    throw new AppError("This products Id does not exist", 404)
+  };
+  
+  return deleted;
+};
+
 module.exports = {
-  createOneProduct
+  createOneProduct,
+  getAllProducts,
+  getOneProduct,
+  editOneProduct,
+  deleteOneProduct
 };
