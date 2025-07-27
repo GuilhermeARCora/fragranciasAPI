d24ebd13642ccd1f5098cc50c879330bf96ca6c5
Guilherme Corá <guilhermearcora@gmail.com>
2025-07-27 00:19:06 -0300

    added email class to send test emails, production setup not possible yet

--- utils/email.js
+++ utils/email.js
@@ -1,26 +1,58 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
+const { Resend } = require('resend');
 
-const sendEmail = async options => {
-    //Create a transporter
-    const transporter = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
-        auth:{
-            user: process.env.EMAIL_USERNAME,
-            pass: process.env.EMAIL_PASSWORD
-        }
+module.exports = class Email {
+  constructor(user, url = null) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `Fragrâncias <noreply@yourdomain.com>`; //process.env.EMAIL_FROM
+    // this.resend = new Resend(process.env.RESEND_API_KEY);
+  }
+
+  newTransport() {
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD
+      }
     });
-    //Define the email options
-    const mailOptions = {
-        from:'Guilherme <admin@gmail.com> ',
-        to: options.email,
-        subject: options.subject,
-        text: options.message
-    };
-    //Actually send the email
-    await transporter.sendMail(mailOptions);
-};
+  }
 
-module.exports = {
-    sendEmail
+  async send(template, subject) {
+    const html = pug.renderFile(`${__dirname}/../views/emails/${template}.pug`, {
+      firstName: this.firstName,
+      url: this.url,
+      subject
+    });
+
+    // Use Resend for production
+    if (process.env.NODE_ENV === 'PRODUCTION') {
+      return await this.resend.emails.send({
+        from: this.from,
+        to: this.to,
+        subject,
+        html
+      });
+    }
+
+    // Fallback: Nodemailer (Mailtrap, Gmail)
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.convert(html)
+    };
+
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send('welcome', 'Bem vindo ao nosso site!');
+  }
 };
